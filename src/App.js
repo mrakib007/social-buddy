0c16dc25fcbf37a27f4f016a2b26b2e1055f82af
Rakib Hasan <mrakibhasan199744@gmail.com>
2021-03-10 20:20:33 +0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import "./App.css";
+import About from "./Components/About/About";
+import PostDetail from "./Components/PostDetail/PostDetail";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/home">Home2</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+          <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+            <About></About>
+            </Route>
+            <Route path="/home">
+             <Home></Home>
+            </Route>
+            <Route path="/post/:id">
+              <PostDetail></PostDetail>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
